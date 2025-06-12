a6a58a80b13cfc5149a070ac5ad4d1b202d74a77
kritistha <kritistha020@gmail.com>
2025-06-12 15:32:19 +1000

    fix

--- app/app.functions/pokemon.js
+++ app/app.functions/pokemon.js
@@ -1,23 +1,25 @@
 const axios = require("axios");
 
-exports.main = (context, sendResponse) => {
-  axios
-    .get(`https://pokeapi.co/api/v2/pokemon/2`)
-    .then(function (response) {
-      return {
-        statusCode: 200,
-        body: { response },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-    })
-    .catch((error) => {
-      return {
-        statusCode:500,
-        body: {
-          error
-        },
-      };
-    });
+exports.main = async (context) => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/2`);
+    console.log("Pokemon API Response:", JSON.stringify(response.data));
+
+    return {
+      statusCode: 200,
+      body: response.data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching pokemon:", error);
+    return {
+      statusCode: 500,
+      body: { error: error.message },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
 };
