ecd3d796b345ba9c158ae426570b60fc87f0bebc
kritistha <kritistha020@gmail.com>
2025-06-12 14:56:34 +1000

    fix fn

--- app/app.functions/joke.js
+++ app/app.functions/joke.js
@@ -2,14 +2,13 @@ const axios = require("axios");
 
 exports.main = (context, sendResponse) => {
   axios
-    .get("https://official-joke-api.appspot.com/random_joke", {
-    })
+    .get("https://official-joke-api.appspot.com/random_joke", {})
     .then(function (response) {
-      console.log('Joke API Response:', JSON.stringify(response.data));
-      return{ body: { response: response.data }, statusCode: 200 };
+      console.log("Joke API Response:", JSON.stringify(response.data));
+      return { body: { response: response.data }, statusCode: 200 };
     })
     .catch(function (error) {
-      console.error('Error fetching joke:', error);
-      return{ body: { error: error.message }, statusCode: 500 };
+      console.error("Error fetching joke:", error);
+      return { body: { error: error.message }, statusCode: 500 };
     });
 };
