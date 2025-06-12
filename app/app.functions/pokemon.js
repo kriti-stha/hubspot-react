da570f043fb08b5a2555c5ff1150fc45e0cea952
kritistha <kritistha020@gmail.com>
2025-06-12 13:38:29 +1000

    change to require

--- app/app.functions/pokemon.js
+++ app/app.functions/pokemon.js
@@ -4,18 +4,18 @@ exports.main = (context, sendResponse) => {
   axios
     .get(`https://pokeapi.co/api/v2/pokemon/2`)
     .then(function (response) {
-      sendResponse({
+      return {
         statusCode: 200,
         body: { response: response.data },
-      });
+      };
     })
     .catch((error) => {
-      sendResponse({
+      return {
         statusCode: error.response?.status || 500,
         body: {
           error:
             error.response?.data?.message || "Failed to fetch Pokemon data",
         },
-      });
+      };
     });
 };
